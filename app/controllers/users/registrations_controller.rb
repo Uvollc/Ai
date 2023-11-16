ae66b84fa5cb7105ee074ba401b70d1cf3ac89bb
mary-nate <mary.nate@square63.com>
2023-11-16 08:44:30 +0500

    request & response format updated

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -9,7 +9,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def respond_with(current_user, _opts = {})
     if resource.persisted?
       render json: {
-        status: {code: 200, message: 'Signed up successfully.'},
+        status: { code: 200, message: 'Signed up successfully.' },
         data: UserSerializer.new(current_user).serializable_hash[:data]
       }, status: :ok
     else
