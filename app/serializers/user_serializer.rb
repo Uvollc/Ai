87276a34763f85b26947ffd77cb8805a1383e3f8
mary-nate <mary.nate@square63.com>
2023-12-29 17:10:44 +0500

    Minor refactor routes

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < BaseSerializer
   set_type :user
 
-  attributes :id, :email, :first_name, :last_name, :phone
+  attributes :id, :email, :first_name, :last_name, :phone, :payment_status
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  root to: 'admin/dashboard#index'
+
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
