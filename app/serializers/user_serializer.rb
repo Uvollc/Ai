359462c9d84121bcc305360d12711beb14c6ad3b
mary-nate <mary.nate@square63.com>
2023-12-29 23:09:08 +0500

    user avatar added

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,7 @@ class UserSerializer < BaseSerializer
   set_type :user
 
   attributes :id, :email, :first_name, :last_name, :phone, :payment_status
+  attribute :avatar do |obj|
+    obj&.avatar&.url
+  end
 end
